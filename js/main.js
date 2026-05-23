8b0f98ad9ad7d022c6a9a3bcc0470d424eac06f6
DCT <dheemanchaitanyathakur@gmail.com>
2026-05-23 17:40:03 +0530

    added a retro-crt look
    will experiment around more, need to add a terminal in between

--- js/main.js
+++ js/main.js
@@ -5,11 +5,26 @@ const emoticons = document.querySelectorAll(".emoticons span")
 emoticons.forEach((face) => {
 
     face.addEventListener("mouseover", () => {
+
         face.style.color = "cyan"
+
     })
 
     face.addEventListener("mouseout", () => {
+
         face.style.color = "#00ff99"
+
     })
 
 })
+
+const bootScreen = document.getElementById("boot-screen")
+const mainSite = document.getElementById("main-site")
+
+setTimeout(() => {
+
+    bootScreen.style.display = "none"
+
+    mainSite.style.display = "block"
+
+}, 3200)
