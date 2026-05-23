843e14c3b751449bc7260f93f0db61dca918ebc5
DCT <dheemanchaitanyathakur@gmail.com>
2026-05-23 21:03:51 +0530

    added a fake boot up sequence
    need to add: more OS stuff, CRT effect possibly

--- js/main.js
+++ js/main.js
@@ -1,30 +1,39 @@
 console.log("> system online :)")
 
 const emoticons = document.querySelectorAll(".emoticons span")
-
 emoticons.forEach((face) => {
-
     face.addEventListener("mouseover", () => {
-
         face.style.color = "cyan"
-
     })
-
     face.addEventListener("mouseout", () => {
-
         face.style.color = "#00ff99"
-
     })
-
 })
 
-const bootScreen = document.getElementById("boot-screen")
-const mainSite = document.getElementById("main-site")
-
-setTimeout(() => {
-
-    bootScreen.style.display = "none"
-
-    mainSite.style.display = "block"
-
-}, 3200)
+const bootInput =
+    document.getElementById("boot-input")
+const bootScreen =
+    document.getElementById("boot-screen")
+const mainSite =
+    document.getElementById("main-site")
+bootInput.focus()
+bootInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        const command =
+            bootInput.value.trim().toLowerCase()
+        if (command === "enter") {
+            bootScreen.classList.add("boot-exit")
+            setTimeout(() => {
+                bootScreen.style.display = "none"
+                mainSite.style.display = "block"
+            }, 2000)
+        }
+        else if (command === "exit") {
+            alert("you cannot leave yet :)")
+        }
+        else {
+            alert("unknown command")
+        }
+        bootInput.value = ""
+    }
+})
