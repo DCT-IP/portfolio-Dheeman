50111ecd636b1a58bbf91d2d0ab209cdb36f20f1
DCT <dheemanchaitanyathakur@gmail.com>
2026-09-22 15:03:47 +0530

    cleaned the js file to remove unecessary stuff

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,20 @@
 console.log("Hai :3, starting ....");
+
 console.log(document.body);
 
 const titleElement = document.getElementById("os-info");
-console.log(titleElement);
+console.log("Title element:", titleElement);
 
-const boxElement = document.querySelector(".box");
-console.log("Box element:", boxElement);
-
-const cursorElement = document.querySelector(".blinking-cursor");
-console.log("Cursor element:", cursorElement);
+const infoBox = document.querySelector(".box");
+console.log("Info box:", infoBox);
 
 const bootButton = document.querySelector("#boot-btn");
-const infoBox = document.querySelector(".box");
 
 bootButton.addEventListener("click", function() {
     titleElement.textContent = "SYSTEM ONLINE";
+
     infoBox.classList.remove("hidden");
+
     bootButton.textContent = "[ ENTER SYSTEM ]";
 });
 
@@ -30,6 +29,8 @@ const centContainer = document.querySelector(".cent");
 
 addLogButton.addEventListener("click", function() {
     const newLog = document.createElement("p");
+
     newLog.textContent = "> System message: Connection secure.";
+
     centContainer.appendChild(newLog);
 });
